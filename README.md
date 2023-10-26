216cefff80485e51a4ed76664b9c2fbf6a5fce82
Caio Dantas <caiodantasl.lara@gmail.com>
2023-10-26 13:18:08 -0300

    create user

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project_agenda
 Projeto desenvolvido para estudar o framework Django 
+    
--- contact/forms.py
+++ contact/forms.py
@@ -52,4 +52,29 @@ class ContactForm(forms.ModelForm):
 
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email', 'username', 'password1', 'password2',
+        )
+
+        def clean_email(self):
+            email = self.cleaned_data.get('email')
+
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                    'email',
+                    ValidationError('Já existe este e-mail', code='invalid')
+                )
+
+                return
